7473a7407ad821587b295ba1ac62e5c7557ac014
Melody <luomel@outlook.com>
2020-03-02 18:48:21 -0500

    Implement Jumping Square

--- src/game.js
+++ src/game.js
@@ -1,9 +1,98 @@
 const Player = require('./player');
 class Game{
     constructor(ctx){
-        this.player = new Player();
-        this.player.show(ctx);
+        this.ctx = ctx;
+        this.dimensions = { width: ctx.canvas.width, height: ctx.canvas.height}
+        // this.player = new Player(this.dimensions);
+        this.spacePressed = false;
+        this.registerEvents();
+        this.restart();
+        this.jump = this.jump.bind(this);
+        this.animate= this.animate.bind(this);
         
+
     }
+    jump(){
+        this.player.jump();
+    }
+    animate(){
+        this.player.animate(this.ctx);
+        // this.background.animate()
+        requestAnimationFrame(this.animate.bind(this));
+    }
+    restart(){
+        this.running = false;
+        this.player = new Player(this.dimensions);
+        //all other things to load here
+        this.animate();
+    }
+    click(e){
+        if (!this.running){
+            this.play();
+        }
+        this.player.jump();
+    }
+    play(){
+        this.running=true;
+        this.animate();
+    }
+    registerEvents() {
+        this.ctx.canvas.addEventListener("keydown",(e) => {
+            // debugger;
+            if (e.keyCode === 32){
+                e.preventDefault();
+                this.running = true;
+                this.click(e);
+            }
+        });
+    }
+    // requestAnimation(){
+    //     debugger;
+    //     window.requestAnimationFrame( ()=>{
+    //         this.player.show(this.ctx);
+    //         if (this.spacePressed){
+    //             this.player.velocity -= 5
+    //             this.spacePressed = false;
+    //         }
+            
+    //         this.player.update();
+            
+    //     });
+    // }
+    
+    
+    
+    // handleSpace(){
+    //     document.addEventListener("keydown", (event)=>{
+    //         if (event.which === "32") {
+    //             this.spacePressed = true;
+    //         }
+    //     }, false);
+    // }
 }
+
+
+
+
+// move(){
+//     this.player.move();
+// }
+// spaceHandler(){
+//     let lastTarget;
+//     window.onload = () => {s
+//         document.addEventListener('mousedown', (event)=>{
+//             lastTarget = event.target;
+//         }, false);
+//         document.addEventListener('keydown', (event)=> {
+//             if (lastTarget === this.canvas) {
+//                 event.preventDefault();
+//                 this.move();
+//                 console.log("hhi");
+//                 debugger;
+//             }
+//         }, false);
+   
+//         }
+//     }
+
 module.exports = Game;
--- src/player.js
+++ src/player.js
@@ -1,33 +1,80 @@
 
 
 class Player {
-    constructor(){
+    constructor(dimensions){
         this.size = 50;
         this.x = this.size; 
-        this.y = 300 - this.size; //it is sitting at the bottom
+        this.y = dimensions.height - this.size; //it is sitting at the bottom
         this.velocity = 0; //velocity speed along the y axis to be able to jump
-
+        this.gravity = .4;
+        this.speed = 1;
+        this.onGround = true;
+        this.spacePressed = false;
     }
-    jump(event){
-        if (event.key === " "|| event.key === "Spacebar"){
-            event.preventDefault();
-            console.log("Space Pressed");
-            this.velocity = -5; //moving it up
-        }
 
-    }
-    move() {
+    
+   jump(){
+       this.velocity = -10;
+       
+   }
+//    fall() {
+//        this.y += 100;
+//    }
+    move(){
         this.y += this.velocity;
+        this.velocity += this.gravity;
     }
-    show(ctx){
+    
+    animate(ctx){
+        this.move();
+        this.draw(ctx);
+    }
+    draw(ctx){
+        ctx.clearRect(0,0,800,300);
         ctx.beginPath();
         ctx.rect(this.x, this.y, this.size, this.size);
+        // ctx.drawImage(this.img, this.x, this.y);
         ctx.fillStyle = "yellow";
         ctx.fill();
         ctx.strokeStyle = 'yellow';
         ctx.stroke();
     }
-    
+
 }
+
+
+// update(){
+//     //are we underground? will adding 30 make is greater than the total height
+//     // debugger;
+//     if (this.y + this.velocity + this.size  + 30 > 300) { 
+//         //if so take us to ground level and reset velocity to 0
+//         this.velocity = 0;
+//         this.y = 300 - this.size - 30;
+//     } else {
+//         // else, move
+//         debugger;
+//         this.velocity += this.gravity;
+//         this.y += this.velocity;
+
+//     }
+// }
+    
+    
+ 
+
+
 module.exports = Player;
 
+// const gravity = 0.4;
+// const initialSpeed = 10;
+// if (this.jumping) {
+//   if (this.jumpCount === 0 || !this.onGround()){
+//     this.position[1] -= initialSpeed - gravity * this.jumpCount;
+//     this.jumpCount += 1;
+//   } else {
+//     this.position[1] = 220;
+//     this.jumpCount = 0;
+//     this.jumping = false;
+//   }
+// }
+// }

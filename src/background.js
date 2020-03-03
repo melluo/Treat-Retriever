1121f133dacdb174ffcfdbd6123a7c16c75c7fea
Melody <luomel@outlook.com>
2020-03-03 14:42:37 -0500

    Add scrolling background

--- src/background.js
+++ src/background.js
@@ -6,7 +6,7 @@ class Background{
         this.image.src = main_background;
         this.imageWidth = this.image.width;
         this.ctxWidth = this.ctx.canvas.width;
-        this.speed = .8;
+        this.speed = 1.5;
         this.x = 0;
         this.y = 0;
         
@@ -16,12 +16,15 @@ class Background{
         // draw 1st image at 0 position
         this.ctx.drawImage(this.image, this.x + this.imageWidth,this.y);
         //draw image 2 for loop (at image width)
-        loop (this.x === this.imageWidth){
+        if (this.x <= (this.imageWidth*-1)){
             //if reach end of image, reset x position to play image again
             this.x = 0;
+    
+            
         } 
         this.x -= this.speed;
         //making image move
+        //negative acceleration is acceleration towards the left
     }
    
 }
